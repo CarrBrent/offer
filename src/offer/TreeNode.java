59443f916bf69e2fafd615ff8944057172ec1112
CarrBrent <958240037@qq.com>
2017-05-14 14:34:47 +0800

    根据遍历结果恢复二叉树

---
+++ src/offer/TreeNode.java
@@ -0,0 +1,12 @@
+package offer;
+
+/**
+ * Created by jiashilin on 2017/5/14.
+ */
+public class TreeNode {
+    int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+
+}
---
+++ src/offer/a04_reConstructBinaryTree.java
@@ -0,0 +1,11 @@
+package offer;
+
+/**
+ * Created by jiashilin on 2017/5/14.
+ */
+public class a04_reConstructBinaryTree {
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        TreeNode treeNode = new TreeNode(0);
+        return treeNode;
+    }
+}
