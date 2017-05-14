fdf27d697fba5ada4a5fb753f9398dd60858ff3f
CarrBrent <958240037@qq.com>
2017-05-14 20:10:24 +0800

    链表节点。

---
+++ src/offer/ListNode.java
@@ -0,0 +1,12 @@
+package offer;
+
+/**
+ * Created by jiashilin on 2017/5/14.
+ */
+public class ListNode {
+        int val;
+        ListNode next = null;
+        ListNode(int val) {
+        this.val = val;
+    }
+}
