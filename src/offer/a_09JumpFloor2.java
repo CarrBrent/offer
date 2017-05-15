676a8e10cbbf8056d11b049cad222c49f29599f7
CarrBrent <958240037@qq.com>
2017-05-15 13:42:40 +0800

    变态跳台阶，实际上计算的是2^(n-1)

---
+++ src/offer/a_09JumpFloor2.java
@@ -0,0 +1,26 @@
+package offer;
+
+/**
+ * Created by jiashilin on 2017/5/15.
+ */
+
+public class a_09JumpFloor2 {
+    public static  int JumpFloorII(int target) {
+        if (target<1) {
+            return 0;
+        }
+        if (target == 1) {
+            return 1;
+        }
+        //通过位移计算2的次幂。但是计算2^0的时候需要单独计算。
+        return 2 <<(target-2);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(JumpFloorII(1));
+        System.out.println(2<<0);
+        System.out.println(2<<1);
+        System.out.println(2<<2);
+    }
+}
