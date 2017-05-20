dc7509050c717fc2ba750a7c609e3bb04e96aa6d
CarrBrent <958240037@qq.com>
2017-05-20 20:27:20 +0800

    复杂链表的复制

---
+++ src/offer/RandomListNode.java
@@ -0,0 +1,14 @@
+package offer;
+
+/**
+ * Created by jiashilin on 2017/5/20.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
