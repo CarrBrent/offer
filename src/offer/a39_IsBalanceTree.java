ea10e7494e821b93f503c00feb358b36a5d34188
CarrBrent <958240037@qq.com>
2017-06-06 19:49:16 +0800

    35.判断是否是平衡二叉树

---
+++ src/offer/a39_IsBalanceTree.java
@@ -0,0 +1,12 @@
+package offer;
+
+/**
+ * Created by jiashilin on 2017/6/6.
+ */
+public class a39_IsBalanceTree {
+    public boolean IsBalanced_Solution(TreeNode root) {
+
+        return true;
+    }
+
+}
