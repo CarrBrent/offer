def7bee2680a39ee918a611e91c6fa2099cccfa3
CarrBrent <958240037@qq.com>
2017-06-07 18:23:53 +0800

    39.判断二叉树是否是平衡二叉树 输入一棵二叉树，判断该二叉树是否是平衡二叉树。

--- src/offer/a39_IsBalanceTree.java
+++ src/offer/a39_IsBalanceTree.java
@@ -5,8 +5,34 @@ package offer;
  */
 public class a39_IsBalanceTree {
     public boolean IsBalanced_Solution(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Depth root_dep = new Depth();
 
-        return true;
+        return isbalanced(root,root_dep);
+    }
+    public class Depth{
+        public int dep;
+    }
+
+    private boolean isbalanced(TreeNode root,Depth depth){
+        if (root == null) {
+            depth.dep = 0;
+            return true;
+        }
+        Depth left_dep = new Depth();
+        Depth right_dep = new Depth();
+        if (isbalanced(root.left,left_dep) &&isbalanced(root.right,right_dep) ) {
+            int different = left_dep.dep - right_dep.dep;
+            if (Math.abs(different) >1) {
+                return false;
+            }else{
+                depth.dep = Math.max(left_dep.dep,right_dep.dep)+1;
+                return true;
+            }
+        }
+        return false;
     }
 
 }
