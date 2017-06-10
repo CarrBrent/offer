1d1003535123f733c585aa00acfad2442f790d2a
CarrBrent <958240037@qq.com>
2017-06-10 16:12:38 +0800

    35.找到两个链表的第一个公共节点

---
+++ src/offer/a46_LastRemaining.java
@@ -0,0 +1,20 @@
+package offer;
+
+/**
+ * Created by jiashilin on 2017/6/9.
+ */
+//约瑟夫问题
+public class a46_LastRemaining {
+    //推导规律实现
+    public int LastRemaining(int n, int m) {
+        if (n<1||m<1){
+            return -1;
+        }
+        int position = 0;
+        for (int i = 2; i <=n ; i++) {
+            position = (position+m)%i;
+        }
+        return position;
+
+    }
+}
